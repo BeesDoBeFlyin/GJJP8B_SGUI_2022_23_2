7e3e23c15b477cde7903c6423a1e9dc5e207dc98
BeesDoBeFlyin <megajewlord@gmail.com>
2021-10-10 22:13:21 +0200

    database location string copied

--- GJJP8B_HFT_2021221.Client/Program.cs
+++ GJJP8B_HFT_2021221.Client/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+//Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="|DataDirectory|\Database1.mdf";Integrated Security=True; MultipleActiveResultSets=True
+
 namespace GJJP8B_HFT_2021221.Client
 {
     class Program
