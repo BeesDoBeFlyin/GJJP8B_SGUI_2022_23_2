a38a9ae44f6315140605ee955bdbe38e6eee5bba
BeesDoBeFlyin <megajewlord@gmail.com>
2021-10-17 13:43:54 +0200

    finished initialisation of test items

--- GJJP8B_HFT_2021221.Data/CheeseContext.cs
+++ GJJP8B_HFT_2021221.Data/CheeseContext.cs
@@ -54,6 +54,7 @@ namespace GJJP8B_HFT_2021221.Data
     {
         static void Main(string[] args)
         {
+            // INITIALISATION
             CheeseContext cd = new CheeseContext();
 
             Milk m1 = new Milk() { Name = "CowMilk", Price = 500 };
@@ -75,8 +76,31 @@ namespace GJJP8B_HFT_2021221.Data
 
             cd.Milks.Add(m1);
             cd.Milks.Add(m2);
+            cd.Cheeses.Add(c1);
+            cd.Cheeses.Add(c2);
+            cd.Cheeses.Add(c3);
+            cd.Buyers.Add(b1);
+            cd.Buyers.Add(b2);
+            cd.Buyers.Add(b3);
 
             cd.SaveChanges();
+
+            Console.WriteLine("Test data created! Listing them now!");
+
+            foreach (var item in cd.Milks)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}");
+            }
+
+            foreach (var item in cd.Cheeses)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}, price {item.Price}, made from {cd.Milks.Find(item.MilkId).Name}");
+            }
+
+            foreach (var item in cd.Buyers)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}, prefers {cd.Cheeses.Find(item.CheeseId).Name}");
+            }
         }
     }
     //static void Main(string[] args)
