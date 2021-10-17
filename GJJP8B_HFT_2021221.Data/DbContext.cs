3a673b1ac5d79f3069ecf5584c0981835dde8f50
BeesDoBeFlyin <megajewlord@gmail.com>
2021-10-17 12:37:48 +0200

    relocated program.cs's commented out content into DbContext.cs (4 failed => 1 failed, progress)

---
+++ GJJP8B_HFT_2021221.Data/DbContext.cs
@@ -0,0 +1,78 @@
+﻿using GJJP8B_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJJP8B_HFT_2021221.Data
+{
+    class DbContext
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating test stuff!");
+
+            MilkContext md = new MilkContext();
+
+            md.Milks.Add(new Milk() { Name = "CowMilk", Price = 500 });
+            md.Milks.Add(new Milk() { Name = "GoatMilk", Price = 700 });
+
+            md.SaveChanges();
+
+            Console.WriteLine("Test milks created!");
+
+            CheeseContext cd = new CheeseContext();
+
+            cd.Cheeses.Add(new Cheese() { Name = "Cheddar", Price = 3500, MilkId = 1 });
+            cd.Cheeses.Add(new Cheese() { Name = "Trapista", Price = 1500, MilkId = 0 });
+            cd.Cheeses.Add(new Cheese() { Name = "Bear", Price = 680, MilkId = 1 });
+
+            cd.SaveChanges();
+
+            Console.WriteLine("Test cheeses created!");
+
+            BuyerContext bd = new BuyerContext();
+
+            bd.Buyers.Add(new Buyer() { Name = "Test Ferenc", CheeseId = 1 });
+            bd.Buyers.Add(new Buyer() { Name = "Teás K. Anna", CheeseId = 0 });
+            bd.Buyers.Add(new Buyer() { Name = "Generic Guy", CheeseId = 2 });
+
+            Console.WriteLine("Test buyers created!");
+
+            // Reading all the created stuff
+            foreach (var item in md.Milks)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}, price {item.Price}.");
+            }
+
+            foreach (var item in cd.Cheeses)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}, price {item.Price} and is made of {item.MilkId}.");
+            }
+
+            foreach (var item in bd.Buyers)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}, cheese preference {item.CheeseId}.");
+            }
+
+            // Purge databases after
+
+            md.Milks.RemoveRange(md.Milks);
+            md.SaveChanges();
+
+            Console.WriteLine("Test milks purged!");
+
+            cd.Cheeses.RemoveRange(cd.Cheeses);
+            cd.SaveChanges();
+
+            Console.WriteLine("Test cheeses purged!");
+
+            bd.Buyers.RemoveRange(bd.Buyers);
+            bd.SaveChanges();
+
+            Console.WriteLine("Test buyers purged!");
+        }
+    }
+}
+
