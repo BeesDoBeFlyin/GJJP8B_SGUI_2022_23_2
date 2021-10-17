a98a64c974ba527d802981c7172df41ca36f996b
BeesDoBeFlyin <megajewlord@gmail.com>
2021-10-17 12:34:36 +0200

    program.cs no longer contains main

--- GJJP8B_HFT_2021221.Client/Program.cs
+++ GJJP8B_HFT_2021221.Client/Program.cs
@@ -7,69 +7,69 @@ namespace GJJP8B_HFT_2021221.Client
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("Creating test stuff!");
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Creating test stuff!");
 
-            //MilkContext md = new MilkContext();
+    //        MilkContext md = new MilkContext();
 
-            //md.Milks.Add(new Milk() { Name = "CowMilk", Price = 500 });
-            //md.Milks.Add(new Milk() { Name = "GoatMilk", Price = 700 });
+    //        md.Milks.Add(new Milk() { Name = "CowMilk", Price = 500 });
+    //        md.Milks.Add(new Milk() { Name = "GoatMilk", Price = 700 });
 
-            //md.SaveChanges();
+    //        md.SaveChanges();
 
-            //Console.WriteLine("Test milks created!");
+    //        Console.WriteLine("Test milks created!");
 
-            //CheeseContext cd = new CheeseContext();
+    //        CheeseContext cd = new CheeseContext();
 
-            //cd.Cheeses.Add(new Cheese() { Name = "Cheddar", Price = 3500, MilkId = 1});
-            //cd.Cheeses.Add(new Cheese() { Name = "Trapista", Price = 1500, MilkId = 0});
-            //cd.Cheeses.Add(new Cheese() { Name = "Bear", Price = 680, MilkId = 1});
+    //        cd.Cheeses.Add(new Cheese() { Name = "Cheddar", Price = 3500, MilkId = 1 });
+    //        cd.Cheeses.Add(new Cheese() { Name = "Trapista", Price = 1500, MilkId = 0 });
+    //        cd.Cheeses.Add(new Cheese() { Name = "Bear", Price = 680, MilkId = 1 });
 
-            //cd.SaveChanges();
+    //        cd.SaveChanges();
 
-            //Console.WriteLine("Test cheeses created!");
+    //        Console.WriteLine("Test cheeses created!");
 
-            //BuyerContext bd = new BuyerContext();
+    //        BuyerContext bd = new BuyerContext();
 
-            //bd.Buyers.Add(new Buyer() { Name = "Test Ferenc", CheeseId = 1 });
-            //bd.Buyers.Add(new Buyer() { Name = "Teás K. Anna", CheeseId = 0 });
-            //bd.Buyers.Add(new Buyer() { Name = "Generic Guy", CheeseId = 2 });
+    //        bd.Buyers.Add(new Buyer() { Name = "Test Ferenc", CheeseId = 1 });
+    //        bd.Buyers.Add(new Buyer() { Name = "Teás K. Anna", CheeseId = 0 });
+    //        bd.Buyers.Add(new Buyer() { Name = "Generic Guy", CheeseId = 2 });
 
-            //Console.WriteLine("Test buyers created!");
+    //        Console.WriteLine("Test buyers created!");
 
-            //// Reading all the created stuff
-            //foreach (var item in md.Milks)
-            //{
-            //    Console.WriteLine($"{item.Id} - {item.Name}, price {item.Price}.");
-            //}
+    //        // Reading all the created stuff
+    //        foreach (var item in md.Milks)
+    //        {
+    //            Console.WriteLine($"{item.Id} - {item.Name}, price {item.Price}.");
+    //        }
 
-            //foreach (var item in cd.Cheeses)
-            //{
-            //    Console.WriteLine($"{item.Id} - {item.Name}, price {item.Price} and is made of {item.MilkId}.");
-            //}
+    //        foreach (var item in cd.Cheeses)
+    //        {
+    //            Console.WriteLine($"{item.Id} - {item.Name}, price {item.Price} and is made of {item.MilkId}.");
+    //        }
 
-            //foreach (var item in bd.Buyers)
-            //{
-            //    Console.WriteLine($"{item.Id} - {item.Name}, cheese preference {item.CheeseId}.");
-            //}
+    //        foreach (var item in bd.Buyers)
+    //        {
+    //            Console.WriteLine($"{item.Id} - {item.Name}, cheese preference {item.CheeseId}.");
+    //        }
 
-            //// Purge databases after
+    //        // Purge databases after
 
-            //md.Milks.RemoveRange(md.Milks);
-            //md.SaveChanges();
+    //        md.Milks.RemoveRange(md.Milks);
+    //        md.SaveChanges();
 
-            //Console.WriteLine("Test milks purged!");
+    //        Console.WriteLine("Test milks purged!");
 
-            //cd.Cheeses.RemoveRange(cd.Cheeses);
-            //cd.SaveChanges();
+    //        cd.Cheeses.RemoveRange(cd.Cheeses);
+    //        cd.SaveChanges();
 
-            //Console.WriteLine("Test cheeses purged!");
+    //        Console.WriteLine("Test cheeses purged!");
 
-            //bd.Buyers.RemoveRange(bd.Buyers);
-            //bd.SaveChanges();
+    //        bd.Buyers.RemoveRange(bd.Buyers);
+    //        bd.SaveChanges();
 
-            //Console.WriteLine("Test buyers purged!");
-        }
-    }
+    //        Console.WriteLine("Test buyers purged!");
+    //    }
+    //}
 }
