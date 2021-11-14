50b79ec340706bba505d74df2e122a55babef1c0
BeesDoBeFlyin <megajewlord@gmail.com>
2021-11-14 10:07:59 +0100

    filled IRepository

--- GJJP8B_HFT_2021221.Repository/IRepository.cs
+++ GJJP8B_HFT_2021221.Repository/IRepository.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace GJJP8B_HFT_2021221.Repository
 {
-    interface IRepository
+    interface IRepository<T> where T : class
     {
+        T ReturnOne(int id);
+
+        IQueryable<T> ReturnAll();
+
+        void Insert(T entity);
+
+        void Delete(int id);
     }
 }
