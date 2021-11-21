bb68f6313b3fab524c874879785dcf08b385ab9c
BeesDoBeFlyin <megajewlord@gmail.com>
2021-11-21 13:26:02 +0100

    Created BuyerLogic

---
+++ GJJP8B_HFT_2021221.Logic/BuyerLogic.cs
@@ -0,0 +1,42 @@
+﻿using GJJP8B_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJJP8B_HFT_2021221.Logic
+{
+    public class BuyerLogic : IBuyerLogic
+    {
+
+        private List<Buyer> Buyers { get; set; }
+
+        public BuyerLogic()
+        {
+            this.Buyers = new List<Buyer>();
+        }
+        public int BuyerCount
+        {
+            get
+            {
+                return this.Buyers.Count;
+            }
+        }
+
+        public void AddBuyer(Buyer buy)
+        {
+            this.Buyers.Add(buy);
+        }
+
+        public List<Buyer> GetAll()
+        {
+            return this.Buyers;
+        }
+
+        public Buyer GetBuyerByIndex(int index)
+        {
+            return this.Buyers[index];
+        }
+    }
+}
