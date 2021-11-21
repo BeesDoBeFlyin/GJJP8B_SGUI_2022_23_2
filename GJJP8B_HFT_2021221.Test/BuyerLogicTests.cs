25b99dd3aa9b40a15b2924d57a13446fa3100ed7
BeesDoBeFlyin <megajewlord@gmail.com>
2021-11-21 17:06:38 +0100

    added check for CheeseId to AddBuyerTest

--- GJJP8B_HFT_2021221.Test/BuyerLogicTests.cs
+++ GJJP8B_HFT_2021221.Test/BuyerLogicTests.cs
@@ -18,13 +18,15 @@ namespace GJJP8B_HFT_2021221.Test
             ILogic.AddBuyer(new Buyer()
             {
                 Name = "Test Buyer #0",
-                Money = 5500
+                Money = 5500,
+                CheeseId = 1
             });
 
             ILogic.AddBuyer(new Buyer()
             {
                 Name = "Test Buyer #1",
-                Money = 7850
+                Money = 7850,
+                CheeseId = 2
             });
         }
 
@@ -33,15 +35,18 @@ namespace GJJP8B_HFT_2021221.Test
         {
             string newName = "Test Buyer #5500";
             int newPrice = 5500;
+            int cheeseid = 1;
 
             ILogic.AddBuyer(new Buyer()
             {
                 Name = newName,
-                Money = newPrice
-            });
+                Money = newPrice,
+                CheeseId = cheeseid
+            }) ;
 
             Assert.That(ILogic.GetAll()[ILogic.BuyerCount - 1].Name, Is.EqualTo(newName));
             Assert.That(ILogic.GetAll()[ILogic.BuyerCount - 1].Money, Is.EqualTo(newPrice));
+            Assert.That(ILogic.GetAll()[ILogic.BuyerCount - 1].CheeseId, Is.EqualTo(cheeseid));
         }
 
         [TestCase(1, "")]
