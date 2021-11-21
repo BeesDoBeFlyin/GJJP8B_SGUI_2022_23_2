690495672912308e213216192d72b98173d5c78f
BeesDoBeFlyin <megajewlord@gmail.com>
2021-11-21 13:36:00 +0100

    Created MilkLogicTests and created a basic test

--- GJJP8B_HFT_2021221.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GJJP8B_HFT_2021221.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ GJJP8B_HFT_2021221.Test/MilkLogicTests.cs
@@ -0,0 +1,46 @@
+﻿using GJJP8B_HFT_2021221.Logic;
+using GJJP8B_HFT_2021221.Models;
+using NUnit.Framework;
+
+namespace GJJP8B_HFT_2021221.Test
+{
+    [TestFixture]
+    public class MilkLogicTests
+    {
+        private IMilkLogic ILogic { get; set; }
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            this.ILogic = new MilkLogic();
+
+            this.ILogic.AddMilk(new Milk()
+            {
+                Name = "Test Milk #0",
+                Price = 500
+            });
+
+            this.ILogic.AddMilk(new Milk()
+            {
+                Name = "Test Milk #1",
+                Price = 1500
+            });
+        }
+
+        [Test]
+        public void AddMilk()
+        {
+            string newName = "Test Milk #5500";
+            int newPrice = 5500;
+
+            ILogic.AddMilk(new Milk()
+            {
+                Name = newName,
+                Price = newPrice
+            });
+
+            Assert.That(ILogic.GetAll()[ILogic.MilkCount - 1].Name, Is.EqualTo(newName));
+            Assert.That(ILogic.GetAll()[ILogic.MilkCount - 1].Price, Is.EqualTo(newPrice));
+        }
+    }
+}
