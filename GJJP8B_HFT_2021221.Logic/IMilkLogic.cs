6511c45446a354ebdad7db37b31a221d466d8a29
BeesDoBeFlyin <megajewlord@gmail.com>
2021-11-21 13:04:03 +0100

    created IMilkLogic

---
+++ GJJP8B_HFT_2021221.Logic/IMilkLogic.cs
@@ -0,0 +1,17 @@
+﻿using GJJP8B_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJJP8B_HFT_2021221.Logic
+{
+    public interface IMilkLogic
+    {
+        int MilkCount { get; }
+        List<Milk> GetAll();
+        void AddMilk(Milk mk);
+        Milk GetMilkById(int id);
+    }
+}
