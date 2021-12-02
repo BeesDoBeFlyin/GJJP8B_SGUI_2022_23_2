f5d2b184f475f0c302b1d2da9cf16b17ab58415e
BeesDoBeFlyin <megajewlord@gmail.com>
2021-12-02 17:54:03 +0100

    rewritten cheese tests in mock

--- GJJP8B_HFT_2021221.Test/CheeseLogicTests.cs
+++ GJJP8B_HFT_2021221.Test/CheeseLogicTests.cs
@@ -1,7 +1,11 @@
 ﻿using GJJP8B_HFT_2021221.Logic;
 using GJJP8B_HFT_2021221.Models;
+using GJJP8B_HFT_2021221.Repository;
+using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GJJP8B_HFT_2021221.Test
 {
@@ -10,63 +14,32 @@ namespace GJJP8B_HFT_2021221.Test
     {
         private CheeseLogic ILogic { get; set; }
 
-        [OneTimeSetUp]
-        public void Setup()
+        private IQueryable<Cheese> FakeCheeses()
         {
-            //this.ILogic = new CheeseLogic();
+            Cheese c1 = new() { Id = 1, Name = "Don't ask", Price = 1500, MilkId = 1, Milk = new Milk(), Buyers = new List<Buyer>() };
+            Cheese c2 = new() { Id = 2, Name = "Trollface", Price = 2500, MilkId = 2, Milk = new Milk(), Buyers = new List<Buyer>() };
+            Cheese c3 = new() { Id = 3, Name = "Ohyes", Price = 850, MilkId = 3, Milk = new Milk(), Buyers = new List<Buyer>() };
 
-            this.ILogic.AddCheese(new Cheese()
-            {
-                Name = "Test Cheese #0",
-                Price = 2500,
-                MilkId = 1
-            });
-            
-            this.ILogic.AddCheese(new Cheese()
-            {
-                Name = "Test Cheese #1",
-                Price = 1950,
-                MilkId = 2
-            });
+            List<Cheese> FakeCheeses = new List<Cheese>();
+
+            FakeCheeses.Add(c1);
+            FakeCheeses.Add(c2);
+            FakeCheeses.Add(c3);
+
+            return FakeCheeses.AsQueryable();
         }
 
-        [Test]
-        public void AddCheeseTest()
+        [SetUp]
+        public void SetUp()
         {
-            string newName = "Test Cheese #5500";
-            int newPrice = 5500;
-            int milkid = 1;
+            Mock<ICheeseRepository> mockedCheeseRepository = new();
+            mockedCheeseRepository.Setup(x => x.ReturnOne(It.IsAny<int>())).Returns<int>((id) => FakeCheeses().FirstOrDefault(x => x.Id == id));
+            mockedCheeseRepository.Setup(x => x.ChangeName(It.IsNotIn<int>(1, 2), It.IsAny<string>())).Throws(new Exception());
+            mockedCheeseRepository.Setup(x => x.ReturnAll()).Returns(FakeCheeses());
 
-            ILogic.AddCheese(new Cheese()
-            {
-                Name = newName,
-                Price = newPrice,
-                MilkId = milkid
-            });
-
-            //Assert.That(ILogic.GetAll()[ILogic.CheeseCount - 1].Name, Is.EqualTo(newName));
-            //Assert.That(ILogic.GetAll()[ILogic.CheeseCount - 1].Price, Is.EqualTo(newPrice));
-            //Assert.That(ILogic.GetAll()[ILogic.CheeseCount - 1].MilkId, Is.EqualTo(milkid));
+            this.ILogic = new CheeseLogic(mockedCheeseRepository.Object);
         }
 
-        [TestCase(1, "")]
-        [TestCase(2, null)]
-        [TestCase(3, "")]
-        public void ChangeCheeseNameTestForNullAndEmptyName(int id, string input)
-        {
-            Assert.That(() => ILogic.ChangeCheeseName(id, input), Throws.TypeOf<Exception>());
-        }
-
-        [TestCase(2, "Test Cheese #0")]
-        public void ChangeMilkNameTestForExistingName(int id, string input)
-        {
-            Assert.That(() => ILogic.ChangeCheeseName(id, input), Throws.TypeOf<Exception>());
-        }
-
-        [TestCase(1, "HHObwwS8Zr21tnxwEG115tQnFrrwQ1J13v050ZAmpREoCr64hBHJkooRPx0TVb9E8DUTwVIkrAqGMR0p9")]
-        public void ChangeCheeseNameTestForCharacterLength(int id, string input)
-        {
-            Assert.That(() => ILogic.ChangeCheeseName(id, input), Throws.TypeOf<Exception>());
-        }
+        
     }
 }
