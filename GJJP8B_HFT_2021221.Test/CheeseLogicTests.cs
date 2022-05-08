0cb760f18a66042e4389e718d294748bfcdadb26
BeesDoBeFlying <megajewlord@gmail.com>
2022-05-08 15:22:05 +0200

    refactored tests

--- GJJP8B_HFT_2021221.Test/CheeseLogicTests.cs
+++ GJJP8B_HFT_2021221.Test/CheeseLogicTests.cs
@@ -48,55 +48,39 @@ namespace GJJP8B_HFT_2021221.Test
         [SetUp]
         public void SetUp()
         {
-            Mock<IMilkRepository> mockedMilkRepository = new();
+            Mock<IRepository<Milk>> mockedMilkRepository = new();
             mockedMilkRepository.Setup(x => x.ReturnOne(It.IsAny<int>())).Returns<int>((id) => FakeMilks().FirstOrDefault(x => x.Id == id));
-            mockedMilkRepository.Setup(x => x.ChangeName(It.IsNotIn<int>(1, 2), It.IsAny<string>())).Throws(new Exception());
             mockedMilkRepository.Setup(x => x.ReturnAll()).Returns(FakeMilks());
 
             this.MLogic = new MilkLogic(mockedMilkRepository.Object);
 
-            Mock<ICheeseRepository> mockedCheeseRepository = new();
+            Mock<IRepository<Cheese>> mockedCheeseRepository = new();
             mockedCheeseRepository.Setup(x => x.ReturnOne(It.IsAny<int>())).Returns<int>((id) => FakeCheeses().FirstOrDefault(x => x.Id == id));
-            mockedCheeseRepository.Setup(x => x.ChangeName(It.IsNotIn<int>(1, 2), It.IsAny<string>())).Throws(new Exception());
             mockedCheeseRepository.Setup(x => x.ReturnAll()).Returns(FakeCheeses());
 
             this.ILogic = new CheeseLogic(mockedCheeseRepository.Object, mockedMilkRepository.Object);
         }
 
         [Test]
-        public void ReturnCheesePriceTest()
+        public void GetOneCheeseTest()
         {
-            float test = ILogic.ReturnPrice(1);
+            Cheese test = ILogic.GetOne(1);
 
-            float expectedResult = 1500f;
+            Cheese expectedResult = new() { Id = 1, Name = "Don't ask", Price = 1500, MilkId = 1, Milk = new Milk(), BuyersNonDb = new List<Buyer>() };
 
             Assert.AreEqual(expectedResult, test);
-        }
-
-        [Test]
-        public void GetCheeseByIdTest()
-        {
-            Cheese test = ILogic.GetCheeseById(1);
-
-            Cheese expectedResult = new Cheese() { Id = 1, Name = "Don't ask", Price = 1500, MilkId = 1, Milk = new Milk(), BuyersNonDb = new List<Buyer>() };
-
             Assert.IsTrue(test.Id == expectedResult.Id);
             Assert.IsTrue(test.Name == expectedResult.Name);
             Assert.IsTrue(test.Price == expectedResult.Price);
         }
 
         [Test]
-        public void ChangeCheeseNameTest()
+        public void UpdateCheeseTest()
         {
-            Assert.Throws(typeof(Exception), () => ILogic.ChangeCheeseName(69, "There is no Milk with such ID!"));
-            Assert.DoesNotThrow(() => ILogic.ChangeCheeseName(1, "Is of workings!"));
-        }
-
-        [Test]
-        public void ChangeCheesePriceTest()
-        {
-            //Assert.Throws(typeof(InvalidOperationException), () => ILogic.ChangePrice(70, 500f)); same stuff, different class
-            Assert.DoesNotThrow(() => ILogic.ChangePrice(1, 500f));
+            Cheese cTest = new() { Id = 50, Name = "Don't ask", Price = 1500, MilkId = 1, Milk = new Milk(), BuyersNonDb = new List<Buyer>() };
+            Assert.Throws(typeof(Exception), () => ILogic.Update(cTest));
+            Cheese cTest2 = new() { Id = 1, Name = "Don't ask", Price = 1500, MilkId = 1, Milk = new Milk(), BuyersNonDb = new List<Buyer>() };
+            Assert.DoesNotThrow(() => ILogic.Update(cTest2));
         }
 
         [Test]
