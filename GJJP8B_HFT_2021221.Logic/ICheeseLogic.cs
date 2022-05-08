50b05f8628259976aa30a19b741f1f73aab4b540
BeesDoBeFlying <megajewlord@gmail.com>
2022-05-08 14:31:55 +0200

    refactored the logic interfaces

--- GJJP8B_HFT_2021221.Logic/ICheeseLogic.cs
+++ GJJP8B_HFT_2021221.Logic/ICheeseLogic.cs
@@ -10,14 +10,11 @@ namespace GJJP8B_HFT_2021221.Logic
 {
     public interface ICheeseLogic
     {
+        void Create(Cheese cheese);
+        void Delete(int id);
+        Cheese GetOne(int id);
         IQueryable<Cheese> GetAll();
-        void AddCheese(Cheese ch);
-        void ChangeCheeseName(int id, string newName);
-        Cheese GetCheeseById(int id);
-        void DeleteCheeseById(int id);
-        void ChangePrice(int id, float newPrice);
-        void ChangeMilk(int id, int newMilkId);
-        float ReturnPrice(int id);
+        void Update(Cheese cheese);
         IEnumerable<Cheese> CheesesUnderPrice(float price);
         IEnumerable<Cheese> CheesesAtPrice(float price);
         IEnumerable<Cheese> CheesesAbovePrice(float price);
